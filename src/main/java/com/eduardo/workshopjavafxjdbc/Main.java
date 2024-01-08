d1e46f2c9baab546d33369c997282e1ea1ac3909
Vitor221 <vitornascimento6650@gmail.com>
2024-01-08 20:57:15 -0300

    Main view design

--- src/main/java/com/eduardo/workshopjavafxjdbc/Main.java
+++ src/main/java/com/eduardo/workshopjavafxjdbc/Main.java
@@ -3,6 +3,7 @@ package com.eduardo.workshopjavafxjdbc;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,13 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("gui/main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        ScrollPane scrollPane = fxmlLoader.load();
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+
+        Scene scene = new Scene(scrollPane);
+
         stage.setScene(scene);
         stage.show();
     }
